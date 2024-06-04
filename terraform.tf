92a1be74c6de8c1443304e02a7c0ab3f3908c3ff
theonlybp <brijeshpatel@outlook.in>
2024-06-04 19:55:19 +0530

    init

---
+++ terraform.tf
@@ -0,0 +1,22 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+
+  # cloud {
+  #   organization = "bp_org"
+
+  #   workspaces {
+  #     name = "learn-terraform"
+  #   }
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
